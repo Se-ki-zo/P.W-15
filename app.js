95344aa4add6fe8cbbba5d18a1504bcb0ea4c9cb
Sekizo <urfill21@gmail.com>
2020-10-19 16:54:03 +0300

    D:/Git/crash-test

--- app.js
+++ app.js
@@ -39,6 +39,12 @@ app.use(cookieParser());
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.post('/signin', login); // test
 app.post('/signup', createUser); // test
 
